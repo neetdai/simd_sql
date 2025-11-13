64b297a17f738d44b6e32f2c2803701352765985
neetdai <>
2025-11-14 00:25:58 +0800

    修改性能测试用例

--- benches/parser_bench.rs
+++ benches/parser_bench.rs
@@ -1,15 +1,19 @@
-use criterion::{Criterion, criterion_group, criterion_main};
+use criterion::{BenchmarkId, Criterion, Throughput, criterion_group, criterion_main};
 use simd_sql::Parser;
 use std::hint::black_box;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("sql parser 1", |b| {
-        let parser = Parser::new().unwrap();
-        b.iter(|| {
-            let sql = "SELECT * FROM table WHERE id = 1";
+    let mut group = c.benchmark_group("sql parser 1");
 
-            parser.parse(&sql).unwrap();
-        })
+    let parser = Parser::new().unwrap();
+
+    let sql_1 = "SELECT * FROM table WHERE id = 1";
+    let sql_len_1 = sql_1.len();
+    group.throughput(Throughput::Elements(sql_1.len() as u64));
+    group.bench_with_input(BenchmarkId::new("sql parser 1", sql_len_1), sql_1, |b, i| {
+        b.iter(|| {
+            parser.parse(&i).unwrap();
+        });
     });
 }
 

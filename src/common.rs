9301921f7d80675f27c2066c3dd107160ff94fe1
neetdai <neetdai>
2026-01-18 17:19:54 +0800

    添加四则运算

--- src/common.rs
+++ src/common.rs
@@ -2,7 +2,7 @@ use std::{alloc::Allocator, borrow::Cow};
 
 use minivec::MiniVec;
 
-use crate::{ParserError, keyword::Keyword, token::{TokenKind, TokenTable}};
+use crate::{ParserError, keyword::Keyword, token::{self, TokenKind, TokenTable}};
 
 pub(crate) fn expect_kind(token_table: &TokenTable, cursor: &usize, token_kind: &TokenKind) -> Result<(), ParserError> {
     if let Some(kind) = token_table.get_kind(*cursor) {
@@ -19,6 +19,8 @@ pub enum Expr {
     Field(Field),
     FunctionCall(Alias<FunctionCall>),
     StringLiteral(StringLiteral),
+    NumbericLiteral(NumbericLiteral),
+    FourBasic(),
 }
 
 impl Expr {
@@ -38,10 +40,25 @@ impl Expr {
         StringLiteral::from_token(token_table, cursor).map(Expr::StringLiteral)
     }
 
+    pub(crate) fn class_number_literal(token_table: &TokenTable, cursor: &mut usize) -> Result<Self, ParserError> {
+        NumbericLiteral::from_token(token_table, cursor).map(Expr::NumbericLiteral)
+    }
+
     pub(crate) fn build(token_table: &TokenTable, cursor: &mut usize) -> Result<Self, ParserError> {
         match token_table.get_kind(*cursor) {
             Some(TokenKind::Number) => {
-                todo!()
+                let number = Self::class_number_literal(token_table, cursor)?;
+
+                match token_table.get_kind(*cursor) {
+                    Some(TokenKind::Plus | TokenKind::Subtract | TokenKind::Divide | TokenKind::Multiply | TokenKind::Mod) => {
+
+                    }
+                    _ => {
+                        todo!()
+                    }
+                }
+
+                Ok(number)
             }
             Some(TokenKind::StringLiteral) => {
                 Self::class_string_literal(token_table, cursor)
@@ -69,7 +86,7 @@ trait FromToken {
 
 #[derive(Debug, PartialEq)]
 pub struct Alias<T> {
-    name: Option<(usize, usize)>,
+    name: Option<usize>,
     value: T,
 }
 
@@ -79,17 +96,19 @@ impl<T> FromToken for Alias<T> where T: FromToken {
         match token_table.get_kind(*cursor) {
             Some(TokenKind::Keyword(Keyword::As)) => {
                 *cursor += 1;
-                if let Some((TokenKind::Identifier, (start, end))) = token_table.get_entry(*cursor) {
+                if let Some(TokenKind::Identifier) = token_table.get_kind(*cursor) {
+                    let name = *cursor;
                     *cursor += 1;
-                    Ok(Alias { name: Some((*start, *end)), value })
+                    Ok(Alias { name: Some(name), value })
                 } else {
                     Err(ParserError::SyntaxError(*cursor, *cursor))
                 }
             }
             Some(TokenKind::Identifier) => {
-                if let Some((TokenKind::Identifier, (start, end))) = token_table.get_entry(*cursor) {
+                if let Some(TokenKind::Identifier) = token_table.get_kind(*cursor) {
+                    let name = *cursor;
                     *cursor += 1;
-                    Ok(Alias { name: Some((*start, *end)), value })
+                    Ok(Alias { name: Some(name), value })
                 } else {
                     Ok(Alias { name: None, value })
                 }
@@ -103,116 +122,234 @@ impl<T> FromToken for Alias<T> where T: FromToken {
 
 #[derive(Debug, PartialEq)]
 pub struct Field {
-    prefix: Option<(usize, usize)>,
-    value: (usize, usize),
+    prefix: Option<usize>,
+    value: usize,
 }
 
 impl FromToken for Field {
     fn from_token(token_table: &TokenTable, cursor: &mut usize) -> Result<Self, ParserError> where Self: Sized {
-        match token_table.get_entry(*cursor) {
-            Some((TokenKind::Identifier, (current_start, current_end))) => {
-                *cursor += 1;
-                if let Some([TokenKind::Dot, TokenKind::Identifier]) = token_table.get_kind(*cursor..=*cursor + 1) {
-                    *cursor += 1;
-                    let (start, end) = token_table.get_position(*cursor).unwrap();
-                    *cursor += 1;
-                    Ok(Self { prefix: Some((*current_start, *current_end)), value: (*start, *end) })
-                } else {
-                    Ok(Self { prefix: None, value: (*current_start, *current_end) })
-                }
-            }
-            _ => Err(ParserError::SyntaxError(*cursor, *cursor)),
-        }
+        let first = token_table.get_kind(*cursor).map(|kind| kind == &TokenKind::Identifier).unwrap_or(false);
+        let dot = token_table.get_kind(*cursor + 1).map(|kind| kind == &TokenKind::Dot).unwrap_or(false);
+        let second = token_table.get_kind(*cursor + 2).map(|kind| kind == &TokenKind::Identifier).unwrap_or(false);
+
+        let sum = (first as usize) + (dot as usize) + (second as usize);
+
+        let (prefix, value) = match (first, sum) {
+            (true, 1) => (None, *cursor),
+            (true, 3) => (Some(*cursor), *cursor + 2),
+            _ => return Err(ParserError::SyntaxError(*cursor, *cursor)),
+        };
+
+        *cursor += sum;
+
+        Ok(Self {
+            prefix,
+            value,
+        })
+
+
+        // match token_table.get_entry(*cursor) {
+        //     Some((TokenKind::Identifier, (current_start, current_end))) => {
+        //         *cursor += 1;
+        //         if let Some([TokenKind::Dot, TokenKind::Identifier]) = token_table.get_kind(*cursor..=*cursor + 1) {
+        //             *cursor += 1;
+        //             let (start, end) = token_table.get_position(*cursor).unwrap();
+        //             *cursor += 1;
+        //             Ok(Self { prefix: Some((*current_start, *current_end)), value: (*start, *end) })
+        //         } else {
+        //             Ok(Self { prefix: None, value: (*current_start, *current_end) })
+        //         }
+        //     }
+        //     _ => Err(ParserError::SyntaxError(*cursor, *cursor)),
+        // }
     }
 }
 
 #[derive(Debug, PartialEq)]
 pub struct Column {
-    prefix: Option<(usize, usize)>,
-    value: (usize, usize),
+    prefix: Option<usize>,
+    value: usize,
 }
 
 impl FromToken for Column {
     fn from_token(token_table: &TokenTable, cursor: &mut usize) -> Result<Self, ParserError> where Self: Sized {
-        match token_table.get_entry(*cursor) {
-            Some((TokenKind::Identifier, (current_start, current_end))) => {
-                *cursor += 1;
-                if let Some([TokenKind::Dot, TokenKind::Identifier]) = token_table.get_kind(*cursor..=*cursor + 1) {
-                    *cursor += 1;
-                    let (start, end) = token_table.get_position(*cursor).unwrap();
-                    *cursor += 1;
-                    Ok(Self { prefix: Some((*current_start, *current_end)), value: (*start, *end) })
-                } else {
-                    Ok(Self { prefix: None, value: (*current_start, *current_end) })
-                }
-            }
-            _ => Err(ParserError::SyntaxError(*cursor, *cursor)),
-        }
+        let first = token_table.get_kind(*cursor).map(|kind| kind == &TokenKind::Identifier).unwrap_or(false);
+        let dot = token_table.get_kind(*cursor + 1).map(|kind| kind == &TokenKind::Dot).unwrap_or(false);
+        let second = token_table.get_kind(*cursor + 2).map(|kind| kind == &TokenKind::Identifier).unwrap_or(false);
+
+        let sum = (first as usize) + (dot as usize) + (second as usize);
+
+        let (prefix, value) = match (first, sum) {
+            (true, 1) => (None, *cursor),
+            (true, 3) => (Some(*cursor), *cursor + 2),
+            _ => return Err(ParserError::SyntaxError(*cursor, *cursor)),
+        };
+
+        *cursor += sum;
+
+        Ok(Self {
+            prefix,
+            value,
+        })
+        
+        // match token_table.get_entry(*cursor) {
+        //     Some((TokenKind::Identifier, (current_start, current_end))) => {
+        //         *cursor += 1;
+        //         if let Some([TokenKind::Dot, TokenKind::Identifier]) = token_table.get_kind(*cursor..=*cursor + 1) {
+        //             *cursor += 1;
+        //             let (start, end) = token_table.get_position(*cursor).unwrap();
+        //             *cursor += 1;
+        //             Ok(Self { prefix: Some((*current_start, *current_end)), value: (*start, *end) })
+        //         } else {
+        //             Ok(Self { prefix: None, value: (*current_start, *current_end) })
+        //         }
+        //     }
+        //     _ => Err(ParserError::SyntaxError(*cursor, *cursor)),
+        // }
     }
 }
 
 #[derive(Debug, PartialEq)]
 pub struct FunctionCall {
-    name: (usize, usize),
+    name: usize,
     args: MiniVec<Expr>,
 }
 
 impl FromToken for FunctionCall {
     fn from_token(token_table: &TokenTable, cursor: &mut usize) -> Result<Self, ParserError> where Self: Sized {
-        if let Some((TokenKind::Identifier, (start, end))) = token_table.get_entry(*cursor) {
-            *cursor += 1;
-            let name = (*start, *end);
+        let first = token_table.get_kind(*cursor).map(|kind| kind == &TokenKind::Identifier).unwrap_or(false);
+        let second = token_table.get_kind(*cursor + 1).map(|kind| kind == &TokenKind::LeftParen).unwrap_or(false);
 
-            expect_kind(token_table, cursor, &TokenKind::LeftParen)?;
-            *cursor += 1;
+        if !(first && second) {
+            return Err(ParserError::SyntaxError(*cursor, *cursor));
+        }
 
-            let mut args = MiniVec::with_capacity(8);
-            loop {
-                let expr = Expr::build(token_table, cursor)?;
-                args.push(expr);
+        let name_pos = *cursor;
+        *cursor += 2;
 
-                if let Some(TokenKind::Comma) = token_table.get_kind(*cursor) {
+        let mut args = MiniVec::with_capacity(8);
+        loop {
+            let expr = Expr::build(token_table, cursor)?;
+            args.push(expr);
+
+            match token_table.get_kind(*cursor) {
+                Some(TokenKind::Comma) => {
                     *cursor += 1;
                     continue;
-                } else if let Some(TokenKind::RightParen) = token_table.get_kind(*cursor) {
+                }
+                Some(TokenKind::RightParen) => {
                     *cursor += 1;
                     break;
-                } else {
+                }
+                _ => {
                     return Err(ParserError::SyntaxError(*cursor, *cursor));
                 }
             }
-
-            Ok(Self {
-                name: name,
-                args,
-            })
-        } else {
-            return Err(ParserError::SyntaxError(*cursor, *cursor));
         }
+
+        Ok(Self {
+            name: name_pos,
+            args,
+        })
+
+        // if let Some((TokenKind::Identifier, (start, end))) = token_table.get_entry(*cursor) {
+        //     *cursor += 1;
+        //     let name = (*start, *end);
+
+        //     expect_kind(token_table, cursor, &TokenKind::LeftParen)?;
+        //     *cursor += 1;
+
+        //     let mut args = MiniVec::with_capacity(8);
+        //     loop {
+        //         let expr = Expr::build(token_table, cursor)?;
+        //         args.push(expr);
+
+        //         if let Some(TokenKind::Comma) = token_table.get_kind(*cursor) {
+        //             *cursor += 1;
+        //             continue;
+        //         } else if let Some(TokenKind::RightParen) = token_table.get_kind(*cursor) {
+        //             *cursor += 1;
+        //             break;
+        //         } else {
+        //             return Err(ParserError::SyntaxError(*cursor, *cursor));
+        //         }
+        //     }
+
+        //     Ok(Self {
+        //         name: name,
+        //         args,
+        //     })
+        // } else {
+        //     return Err(ParserError::SyntaxError(*cursor, *cursor));
+        // }
     }
 }
 
 #[derive(Debug, PartialEq)]
 pub struct StringLiteral {
-    value: (usize, usize),
+    value: usize,
 }
 
 impl FromToken for StringLiteral {
     fn from_token(token_table: &TokenTable, cursor: &mut usize) -> Result<Self, ParserError> where Self: Sized {
-        if let Some((TokenKind::StringLiteral, (start, end))) = token_table.get_entry(*cursor) {
+        if let Some(TokenKind::StringLiteral) = token_table.get_kind(*cursor) {
+            let value = *cursor;
             *cursor += 1;
-            Ok(Self { value: (*start, *end) })
+            Ok(Self { value  })
         } else {
             Err(ParserError::SyntaxError(*cursor, *cursor))
         }
     }
 }
 
+#[derive(Debug, PartialEq)]
+pub struct NumbericLiteral {
+    value: usize,
+}
+
+impl FromToken for NumbericLiteral {
+    fn from_token(token_table: &TokenTable, cursor: &mut usize) -> Result<Self, ParserError> where Self: Sized {
+        if let Some(TokenKind::Number) = token_table.get_kind(*cursor) {
+            let value = *cursor;
+            *cursor += 1;
+            Ok(Self { value  })
+        } else {
+            Err(ParserError::SyntaxError(*cursor, *cursor))
+        }
+    }
+}
+
+// 四则运算
+#[derive(Debug, PartialEq)]
+pub enum FourBasic {
+    Add {
+        left: Box<Expr>,
+        right: Box<Expr>,
+    },
+    Subtract {
+        left: Box<Expr>,
+        right: Box<Expr>,
+    },
+    Multiply {
+        left: Box<Expr>,
+        right: Box<Expr>,
+    },
+    Divide {
+        left: Box<Expr>,
+        right: Box<Expr>,
+    },
+}
+
+impl FourBasic {
+    
+}
+
+
 #[cfg(test)]
 mod test {
     use minivec::mini_vec;
 
-    use crate::{Expr, common::{Alias, Column, FunctionCall, StringLiteral}, keyword::Keyword, token::{TokenKind, TokenTable}};
+    use crate::{Expr, ParserError, common::{Alias, Column, FunctionCall, StringLiteral}, keyword::Keyword, token::{TokenKind, TokenTable}};
 
 
     #[test]
@@ -227,8 +364,8 @@ mod test {
         assert_eq!(expr, Expr::Column(Alias {
             name: None,
             value:  Column {
-                prefix: Some((0, 1)),
-                value: (3, 4),
+                prefix: Some(0),
+                value: 2,
             }
         }));
         assert_eq!(cursor, 3);
@@ -243,10 +380,10 @@ mod test {
         let mut cursor = 0;
         let expr = Expr::class_column(&token_table, &mut cursor).unwrap();
         assert_eq!(expr, Expr::Column(Alias {
-            name: Some((7, 8)),
+            name: Some(4),
             value:  Column {
-                prefix: Some((0, 1)),
-                value: (3, 4),
+                prefix: Some(0),
+                value: 2,
             }
         }));
         assert_eq!(cursor, 5);
@@ -260,10 +397,10 @@ mod test {
         let mut cursor = 0;
         let expr = Expr::class_column(&token_table, &mut cursor).unwrap();
         assert_eq!(expr, Expr::Column(Alias {
-            name: Some((5, 6)),
+            name: Some(3),
             value:  Column {
-                prefix: Some((0, 1)),
-                value: (3, 4),
+                prefix: Some(0),
+                value: 2,
             }
         }));
         assert_eq!(cursor, 4);
@@ -280,12 +417,54 @@ mod test {
         let mut cursor = 0;
         let expr = Expr::class_function_call(&token_table, &mut cursor).unwrap();
         assert_eq!(expr, Expr::FunctionCall(Alias { name: None, value: FunctionCall {
-            name: (0, 1),
+            name: 0,
             args: mini_vec![
                 Expr::StringLiteral(StringLiteral {
-                    value: (3, 5)
+                    value: 2
                 })
             ]
-        } }))
+        } }));
+        assert_eq!(cursor, 4);
+    }
+
+    #[test]
+    fn test_function_1() {
+        let mut token_table = TokenTable::with_capacity(3);
+        token_table.push(TokenKind::Identifier, 0, 1); // name
+        token_table.push(TokenKind::LeftParen, 2, 2);
+        token_table.push(TokenKind::StringLiteral, 3,5);
+        token_table.push(TokenKind::Comma, 6, 6);
+        token_table.push(TokenKind::StringLiteral, 7,8);
+        token_table.push(TokenKind::RightParen, 9, 9); // args
+
+        let mut cursor = 0;
+        let expr = Expr::class_function_call(&token_table, &mut cursor).unwrap();
+        assert_eq!(expr, Expr::FunctionCall(Alias { name: None, value: FunctionCall {
+            name: 0,
+            args: mini_vec![
+                Expr::StringLiteral(StringLiteral {
+                    value: 2
+                }),
+                Expr::StringLiteral(StringLiteral {
+                    value: 4
+                })
+            ]
+        } }));
+        assert_eq!(cursor, 6);
+    }
+
+    #[test]
+    fn test_function_should_panic_1()
+    {
+        let mut token_table = TokenTable::with_capacity(3);
+        token_table.push(TokenKind::Identifier, 0, 1); // name
+        token_table.push(TokenKind::LeftParen, 2, 2);
+        token_table.push(TokenKind::StringLiteral, 3,5);
+        token_table.push(TokenKind::Comma, 6, 6);
+        token_table.push(TokenKind::RightParen, 7, 7); // args
+
+        let mut cursor = 0;
+        let expr = Expr::class_function_call(&token_table, &mut cursor);
+        assert_eq!(expr, Err(ParserError::SyntaxError(4, 4)));
     }
 }
